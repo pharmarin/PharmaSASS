zhiyuanzmj/bootstrap

chore(update): bump to 4.1.3

build/build-plugins.js
@@ -28,6 +28,11 @@ const plugins = [
 const format = 'umd'
 const rootPath = !TEST ? '../js/dist/' : '../js/coverage/dist/'
 const bsPlugins = {
+  Data: path.resolve(__dirname, '../js/src/dom/data.js'),
+  EventHandler: path.resolve(__dirname, '../js/src/dom/eventHandler.js'),
+  Manipulator: path.resolve(__dirname, '../js/src/dom/manipulator.js'),
+  Polyfill: path.resolve(__dirname, '../js/src/dom/polyfill.js'),
+  SelectorEngine: path.resolve(__dirname, '../js/src/dom/selectorEngine.js'),
   Alert: path.resolve(__dirname, '../js/src/alert.js'),
   Button: path.resolve(__dirname, '../js/src/button.js'),
   Carousel: path.resolve(__dirname, '../js/src/carousel.js'),
@@ -41,27 +46,113 @@ const bsPlugins = {
   Util: path.resolve(__dirname, '../js/src/util.js')
 }
 
+const defaultPluginConfig = {
+  external: [
+    bsPlugins.Data,
+    bsPlugins.EventHandler,
+    bsPlugins.SelectorEngine,
+    bsPlugins.Util
+  ],
+  globals: {
+    [bsPlugins.Data]: 'Data',
+    [bsPlugins.EventHandler]: 'EventHandler',
+    [bsPlugins.SelectorEngine]: 'SelectorEngine',
+    [bsPlugins.Util]: 'Util'
+  }
+}
+
+function getConfigByPluginKey(pluginKey) {
+  if (
+    pluginKey === 'Data' ||
+    pluginKey === 'Manipulator' ||
+    pluginKey === 'Util'
+  ) {
+    return {
+      external: [],
+      globals: {}
+    }
+  }
+
+  if (pluginKey === 'EventHandler' || pluginKey === 'SelectorEngine') {
+    return {
+      external: [
+        bsPlugins.Polyfill,
+        bsPlugins.Util
+      ],
+      globals: {
+        [bsPlugins.Polyfill]: 'Polyfill',
+        [bsPlugins.Util]: 'Util'
+      }
+    }
+  }
+
+  if (pluginKey === 'Polyfill') {
+    return {
+      external: [bsPlugins.Util],
+      globals: {
+        [bsPlugins.Util]: 'Util'
+      }
+    }
+  }
+
+  if (pluginKey === 'Alert' || pluginKey === 'Tab') {
+    return defaultPluginConfig
+  }
+
+  if (
+    pluginKey === 'Button' ||
+    pluginKey === 'Carousel' ||
+    pluginKey === 'Collapse' ||
+    pluginKey === 'Modal' ||
+    pluginKey === 'ScrollSpy'
+  ) {
+    const config = Object.assign(defaultPluginConfig)
+    config.external.push(bsPlugins.Manipulator)
+    config.globals[bsPlugins.Manipulator] = 'Manipulator'
+    return config
+  }
+
+  if (pluginKey === 'Dropdown' || pluginKey === 'Tooltip') {
+    const config = Object.assign(defaultPluginConfig)
+    config.external.push(bsPlugins.Manipulator, 'popper.js')
+    config.globals[bsPlugins.Manipulator] = 'Manipulator'
+    config.globals['popper.js'] = 'Popper'
+    return config
+  }
+
+  if (pluginKey === 'Popover') {
+    return {
+      external: [
+        bsPlugins.Data,
+        bsPlugins.SelectorEngine,
+        bsPlugins.Tooltip,
+        bsPlugins.Util
+      ],
+      globals: {
+        [bsPlugins.Data]: 'Data',
+        [bsPlugins.SelectorEngine]: 'SelectorEngine',
+        [bsPlugins.Tooltip]: 'Tooltip',
+        [bsPlugins.Util]: 'Util'
+      }
+    }
+  }
+}
+
 Object.keys(bsPlugins)
   .forEach((pluginKey) => {
     console.log(`Building ${pluginKey} plugin...`)
 
-    const external = ['jquery', 'popper.js']
-    const globals = {
-      jquery: 'jQuery', // Ensure we use jQuery which is always available even in noConflict mode
-      'popper.js': 'Popper'
-    }
+    const config = getConfigByPluginKey(pluginKey)
+    const external = config.external
+    const globals = config.globals
 
-    // Do not bundle Util in plugins
-    if (pluginKey !== 'Util') {
-      external.push(bsPlugins.Util)
-      globals[bsPlugins.Util] = 'Util'
-    }
-
-    // Do not bundle Tooltip in Popover
-    if (pluginKey === 'Popover') {
-      external.push(bsPlugins.Tooltip)
-      globals[bsPlugins.Tooltip] = 'Tooltip'
-    }
+    const pluginPath = [
+      'Data',
+      'EventHandler',
+      'Manipulator',
+      'Polyfill',
+      'SelectorEngine'
+    ].includes(pluginKey) ? `${rootPath}/dom/` : rootPath
 
     rollup.rollup({
       input: bsPlugins[pluginKey],
@@ -73,7 +164,7 @@ Object.keys(bsPlugins)
         name: pluginKey,
         sourcemap: true,
         globals,
-        file: path.resolve(__dirname, `${rootPath}${pluginKey.toLowerCase()}.js`)
+        file: path.resolve(__dirname, `${pluginPath}${pluginKey.toLowerCase()}.js`)
       })
         .then(() => console.log(`Building ${pluginKey} plugin... Done !`))
         .catch((err) => console.error(`${pluginKey}: ${err}`))

pharmarin/PharmaSASS

build/build-plugins.js
@@ -26,6 +26,11 @@ const plugins = [
   })
 ]
 const bsPlugins = {
+  Data: path.resolve(__dirname, '../js/src/dom/data.js'),
+  EventHandler: path.resolve(__dirname, '../js/src/dom/eventHandler.js'),
+  Manipulator: path.resolve(__dirname, '../js/src/dom/manipulator.js'),
+  Polyfill: path.resolve(__dirname, '../js/src/dom/polyfill.js'),
+  SelectorEngine: path.resolve(__dirname, '../js/src/dom/selectorEngine.js'),
   Alert: path.resolve(__dirname, '../js/src/alert.js'),
   Button: path.resolve(__dirname, '../js/src/button.js'),
   Carousel: path.resolve(__dirname, '../js/src/carousel.js'),
@@ -41,26 +46,112 @@ const bsPlugins = {
 }
 const rootPath = TEST ? '../js/coverage/dist/' : '../js/dist/'
 
+const defaultPluginConfig = {
+  external: [
+    bsPlugins.Data,
+    bsPlugins.EventHandler,
+    bsPlugins.SelectorEngine,
+    bsPlugins.Util
+  ],
+  globals: {
+    [bsPlugins.Data]: 'Data',
+    [bsPlugins.EventHandler]: 'EventHandler',
+    [bsPlugins.SelectorEngine]: 'SelectorEngine',
+    [bsPlugins.Util]: 'Util'
+  }
+}
+
+function getConfigByPluginKey(pluginKey) {
+  if (
+    pluginKey === 'Data' ||
+    pluginKey === 'Manipulator' ||
+    pluginKey === 'Util'
+  ) {
+    return {
+      external: [],
+      globals: {}
+    }
+  }
+
+  if (pluginKey === 'EventHandler' || pluginKey === 'SelectorEngine') {
+    return {
+      external: [
+        bsPlugins.Polyfill,
+        bsPlugins.Util
+      ],
+      globals: {
+        [bsPlugins.Polyfill]: 'Polyfill',
+        [bsPlugins.Util]: 'Util'
+      }
+    }
+  }
+
+  if (pluginKey === 'Polyfill') {
+    return {
+      external: [bsPlugins.Util],
+      globals: {
+        [bsPlugins.Util]: 'Util'
+      }
+    }
+  }
+
+  if (pluginKey === 'Alert' || pluginKey === 'Tab') {
+    return defaultPluginConfig
+  }
+
+  if (
+    pluginKey === 'Button' ||
+    pluginKey === 'Carousel' ||
+    pluginKey === 'Collapse' ||
+    pluginKey === 'Modal' ||
+    pluginKey === 'ScrollSpy'
+  ) {
+    const config = Object.assign(defaultPluginConfig)
+    config.external.push(bsPlugins.Manipulator)
+    config.globals[bsPlugins.Manipulator] = 'Manipulator'
+    return config
+  }
+
+  if (pluginKey === 'Dropdown' || pluginKey === 'Tooltip') {
+    const config = Object.assign(defaultPluginConfig)
+    config.external.push(bsPlugins.Manipulator, 'popper.js')
+    config.globals[bsPlugins.Manipulator] = 'Manipulator'
+    config.globals['popper.js'] = 'Popper'
+    return config
+  }
+
+  if (pluginKey === 'Popover') {
+    return {
+      external: [
+        bsPlugins.Data,
+        bsPlugins.SelectorEngine,
+        bsPlugins.Tooltip,
+        bsPlugins.Util
+      ],
+      globals: {
+        [bsPlugins.Data]: 'Data',
+        [bsPlugins.SelectorEngine]: 'SelectorEngine',
+        [bsPlugins.Tooltip]: 'Tooltip',
+        [bsPlugins.Util]: 'Util'
+      }
+    }
+  }
+}
+
 function build(plugin) {
   console.log(`Building ${plugin} plugin...`)
 
-  const external = ['jquery', 'popper.js']
-  const globals = {
-    jquery: 'jQuery', // Ensure we use jQuery which is always available even in noConflict mode
-    'popper.js': 'Popper'
-  }
+  const config = getConfigByPluginKey(plugin)
+  const external = config.external
+  const globals = config.globals
 
-  // Do not bundle Util in plugins
-  if (plugin !== 'Util') {
-    external.push(bsPlugins.Util)
-    globals[bsPlugins.Util] = 'Util'
-  }
-
-  // Do not bundle Tooltip in Popover
-  if (plugin === 'Popover') {
-    external.push(bsPlugins.Tooltip)
-    globals[bsPlugins.Tooltip] = 'Tooltip'
-  }
+  const pluginPath = [
+    'Data',
+    'EventHandler',
+    'Manipulator',
+    'Polyfill',
+    'SelectorEngine'
+  ].includes(plugin) ? `${rootPath}/dom/` : rootPath
 
   const pluginFilename = `${plugin.toLowerCase()}.js`
 
@@ -75,7 +166,7 @@ function build(plugin) {
       name: plugin,
       sourcemap: true,
       globals,
-      file: path.resolve(__dirname, `${rootPath}${pluginFilename}`)
+      file: path.resolve(__dirname, `${pluginPath}${pluginFilename}`)
     })
       .then(() => console.log(`Building ${plugin} plugin... Done!`))
       .catch((err) => console.error(`${plugin}: ${err}`))